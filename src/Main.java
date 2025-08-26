450afd0b8ee26b7f1cc103b9849b1c28f26f4c09
vath <bro.vath7547@gmail.com>
2025-08-26 10:03:55 +0700

    updated styling

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -11,11 +12,16 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
 
         Parent root = FXMLLoader.load(getClass().getResource("/FXML/main.fxml"));
+        Scene scene = new Scene(root);
+
+        // ✅ Load CSS manually from classpath
+        scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
+
+        // Set window icon and title
         Image icon = new Image("/Image/itc.png");
         stage.getIcons().add(icon);
         stage.setTitle("CPU SCHEDULING ALGORITHM SIMULATOR");
-        Scene scene = new Scene(root);
-        
+
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
@@ -25,4 +31,3 @@ public class Main extends Application {
         launch(args);
     }
 }
-
